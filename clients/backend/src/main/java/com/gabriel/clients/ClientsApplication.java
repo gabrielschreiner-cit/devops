969fa9a0bc617f7e15ba77379787dc770efa8bb2
Gabriel Schreiner Bueno de Camargo <gabrielbc@ciandt.com>
2022-08-29 22:45:55 -0300

    added hello world

--- clients/backend/src/main/java/com/gabriel/clients/ClientsApplication.java
+++ clients/backend/src/main/java/com/gabriel/clients/ClientsApplication.java
@@ -8,6 +8,7 @@ public class ClientsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
--- clients/backend/src/test/java/com/gabriel/clients/ClientsApplicationTests.java
+++ clients/backend/src/test/java/com/gabriel/clients/ClientsApplicationTests.java
@@ -8,6 +8,7 @@ class ClientsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Hello, World!");
 	}
 
 }
